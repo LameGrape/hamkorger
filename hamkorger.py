56dcd56c64e958d6b7963296181418b0ce810d76
EpicRaisin <65032841+LameGrape@users.noreply.github.com>
2025-05-14 18:10:09 -0500

    comments!!

--- hamkorger.py
+++ hamkorger.py
@@ -1,6 +1,7 @@
 import struct
 import os
 
+# i cant even lie these classes exists cause its fun writing binary readers and writers
 class BinaryReader:
     def __init__(self, stream):
         self.data = stream.read()
@@ -47,13 +48,18 @@ class BinaryWriter:
     def bool(self, data): self.write(struct.pack(">?", data))
     def string(self, data): self.write(data.encode())
 
+# open a .sav file and read out
 def getSongs(path):
     if not os.path.isfile(path): return None
+
     reader = BinaryReader(open(path, "rb"))
     songs = []
-    reader.skip(4)
+
+    reader.skip(4) # checksum maybe?
     if reader.string(4) != "M01W": return None
-    reader.skip(4)
+    reader.skip(4) # yeah i have no clue but its not important so skip
+
+    # the song names are stored twice? i dont know why but ijust need the names sooo
     for i in range(10):
         song = {
             "modified": reader.bool(),
@@ -63,11 +69,13 @@ def getSongs(path):
             "blockSteps": []
         }
         songs.append(song)
-        reader.skip(31)
+        reader.skip(31) # probably not important, skip
 
     for i, song in enumerate(songs):
-        reader.seek(0x1000 + 0xC000 * i)
-        reader.skip(13)
+        reader.seek(0x1000 + 0xC000 * i) # thank you korg devs for storing song data at constant offsets
+
+        # channel data
+        reader.skip(13) # probably not important, skip
         for i in range(8):
             channel = {
                 "attack": reader.byte(),
@@ -75,85 +83,114 @@ def getSongs(path):
                 "blocks": [],
             }
             song["channels"].append(channel)
-            reader.skip(54)
-        reader.skip(57)
+            reader.skip(54) # probably not important, skip
+        # ive definitely skipped instrument info already but i just havent bothered finding where yet
+
+        reader.skip(57) # probably not important, skip
         song["tempo"] = reader.short()
         song["swing"] = reader.byte()
         song["steps"] = reader.byte()
-        reader.skip(4)
+        reader.skip(4) # probably not important, skip
         musicPos = reader.short() + reader.position()
+
         for i in range(99):
             song["blockTempos"].append(reader.short())
             song["blockSteps"].append(reader.byte())
-            reader.skip(5)
+            reader.skip(5) # probably not important, skip
+
         reader.seek(musicPos)
-        for i in range(99):
+        for i in range(99): ## 99 blocks per 8 channes
             for j in range(8):
-                reader.skip(4)
-                offset = reader.byte()
-                reader.skip(1)
+                reader.skip(4) # probably not important, skip
+                # the channel number is included there but we have that already
+                offset = reader.byte() # offset of block (0 is far left)
+                reader.skip(1) # offset might actually be a short? i dont care, only goes up to 99 anyways
+
                 noteCount = reader.short()
-                if noteCount <= 0: continue
+                if noteCount <= 0: continue # empty :(
+                
                 block = {
                     "offset": offset,
                     "notes": []
                 }
                 for i in range(noteCount):
                     note = {
-                        "length": (reader.byte() + 1) / 4,
-                        "velocity": reader.byte(),
-                        "pitch": reader.byte(),
-                        "offset": reader.byte()
+                        "length": (reader.byte() + 1) / 4, # actual length is multiplied by 4 in the .sav to fit in an integer
+                        "velocity": reader.byte(), # 0-15
+                        "pitch": reader.byte(), # 140-240
+                        "offset": reader.byte() # offset relative to start of the currentblock
                     }
                     block["notes"].append(note)
                 song["channels"][j]["blocks"].append(block)
+    
     return songs
 
+# export a song (you should have gotten one ^ up there) to a midi file
+# if my midi implementation sucks you can blame http://www.music.mcgill.ca/~ich/classes/mumt306/StandardMIDIfileformat.html
 def exportSong(song):
     writer = BinaryWriter(open(f"{song["name"]}.mid", "wb"))
+
+    # midi header
     writer.string("MThd")
-    writer.int(6)
-    writer.short(0)
-    writer.short(1)
-    writer.short(400)
+    writer.int(6) # need to set the length even though the header is always the same size? this format is goofy
+    writer.short(0) # format 0 (only 1 track)
+    writer.short(1) # still need to set the track count :(
+    writer.short(400) # 400 ticks per quarter note
+    # a 1 length M01 note is (i believe) 1/16th beat, so the lowest possible note size is 25 ticks
+
     notes = []
     lastTempo = song["tempo"]
+
     offsets = [0]
     for i, steps in enumerate(song["blockSteps"]):
         offsets.append(offsets[i] + (song["steps"] if song["blockSteps"][i] == 0 else song["blockSteps"][i]))
+
     for i, channel in enumerate(channel for channel in song["channels"] if len(channel["blocks"]) > 0):
         for block in channel["blocks"]:
             tempo = song["tempo"] if song["blockTempos"][block["offset"]] == 0 else song["blockTempos"][block["offset"]]
             if tempo != lastTempo:
-                notes.append((0, offsets[block["offset"]], tempo, -1))
+                notes.append((0, offsets[block["offset"]], tempo, -1)) # negative velocity ensures it comes before notes
             lastTempo = tempo
+            
             for note in block["notes"]:
                 offset = offsets[block["offset"]]
-                notes.append((i, note["offset"] + offset, note["pitch"], note["velocity"]))
-                notes.append((i, note["offset"] + note["length"] + offset, note["pitch"], 0))
-    notes.sort(key=lambda x: x[1] * 100 + x[3] + 1)
+                notes.append((i, note["offset"] + offset, note["pitch"], note["velocity"])) # positive velocity for note on
+                notes.append((i, note["offset"] + note["length"] + offset, note["pitch"], 0)) # zero velocity for note off
+
+    notes.sort(key=lambda x: x[1] * 100 + x[3] + 1) # magic bullcrap to sort them in the right order
+
+    # midi track
     writer.string("MTrk")
     lengthPos = writer.position()
-    writer.pad(4)
+    writer.pad(4) # dont know the length of the data yet, so come back later
+
+    # set the start tempo
     writer.write(bytes((0x00, 0xFF, 0x51, 0x03)))
     writer.write(struct.pack(">I", int(1000**2 / (song["tempo"] / 60)))[1:])
+    # midi tempos are stored as "microseconds per beat", not "beats per minute" for some reason
+
     lastOffset = 0
     for note in notes:
-        writeVarLen((note[1] - lastOffset) * 100, writer)
-        if(note[3] == -1):
+        writeVarLen((note[1] - lastOffset) * 100, writer) # M01 note offsets > MIDI ticks
+
+        if(note[3] == -1): # tempo change
             writer.write(bytes((0xFF, 0x51, 0x03)))
             writer.write(struct.pack(">I", int(1000**2 / (note[2] / 60)))[1:])
-        else:
-            writer.byte((0x90 if note[3] > 0 else 0x80) + note[0])
-            writer.byte(note[2] - 128)
-            writer.byte(int(note[3] / 15 * 127))
+        else: # note
+            writer.byte((0x90 if note[3] > 0 else 0x80) + note[0]) # note on/off and channel
+            writer.byte(note[2] - 128) # pitch
+            writer.byte(int(note[3] / 15 * 127)) # velocity
             lastOffset = note[1]
         pass
-    writer.write(bytes((0x00, 0xFF, 0x2F, 0x00)))
+
+    writer.write(bytes((0x00, 0xFF, 0x2F, 0x00))) # end of track
+
     length = writer.position() - lengthPos - 4
     writer.seek(lengthPos)
-    writer.int(length)
+    writer.int(length)  # i told you wed come back
 
+# a special data type is used for variable length integers
+# adapted from C code provided by the specification
 def writeVarLen(value, writer):
     value = int(value)
     buffer = value & 0x7f
@@ -170,15 +207,18 @@ def writeVarLen(value, writer):
 
 if __name__ == "__main__":
     print("---| hamkorger - Korg M01 MIDI extractor |---\n")
+    
     print("Enter the .sav file path:")
     path = input("> ")
     songs = getSongs(path)
     if songs is None:
         print("Invalid save file")
         exit(1)
+
     print("\nEnter the number of the song to export:")
     for i, song in enumerate(songs):
         print(f"- {i + 1}{"*" if song["modified"] else ""}: {song["name"]}")
     selection = int(input("> ")) - 1
     exportSong(songs[selection])
+
     print(f"Done > {songs[selection]["name"]}.mid")
