dd170a94b9a5c1ad6830b226187ce67d3537c48c
lmdn1 <l@lmdn.cc>
2025-05-22 21:54:22 +0100

    Add attack & release Midi CCs

--- hamkorger.py
+++ hamkorger.py
@@ -196,6 +196,18 @@ def exportSong(song):
     writer.write(struct.pack(">I", int(1000**2 / (song["tempo"] / 60)))[1:])
     # midi tempos are stored as "microseconds per beat", not "beats per minute" for some reason
 
+    midiCCBase = 0xB0
+    midiAttack = 73
+    midiRelease = 72
+
+    def writeMidiCC(channel, control, value):
+        writer.write(bytes((0x00, midiCCBase + channel)))
+        writer.byte(control)
+        # A/R is 0-15 in M01, MIDI CCs are 0-127.
+        # Value scaling should be moved if other CCs
+        # don't follow this.
+        writer.byte(int(value*127/15))
+
     # set instruments to midi equivalents, if notes exist
     for i in range(8):
         if len(song["channels"][i]["blocks"]) == 0:
@@ -204,6 +216,9 @@ def exportSong(song):
         midi_inst_no = instruments[synths[synth_no]][categories[category_no]][inst_no][1]
         writer.write(bytes((0x00, 0xC0 + i))) # program change
         writer.byte(midi_inst_no) # instrument number
+
+        writeMidiCC(i, midiAttack, song["channels"][i]["attack"])
+        writeMidiCC(i, midiRelease, song["channels"][i]["release"])
 
     lastOffset = 0
     for note in notes:
